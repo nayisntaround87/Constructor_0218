959168c568f01196f1e85e2f2e30d68043c61e78
nayisntaround87 <naylasalwahayati@gmail.com>
2024-05-14 14:58:47 +0700

    menambah void

---
+++ mbrStatikfunc.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+private :
+    static int nim;
+
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+
+}
