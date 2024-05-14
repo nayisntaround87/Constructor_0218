40841f7895b94c332d1814c75440230bb71bcf06
nayisntaround87 <naylasalwahayati@gmail.com>
2024-05-14 15:44:44 +0700

    changes 2

--- constructor.cpp
+++ constructor.cpp
@@ -4,6 +4,53 @@ using namespace std;
 
 class mahasiswa
 {
-    public:
+public:
     int nim;
+    string nama;
+
+    
+public:
+    mahasiswa()
+    {
+        nim = 0;
+        nama = " ";
+    };
+
+    mahasiswa(int iNim)
+    {
+        nim = iNim;
+    };
+
+    mahasiswa(string iNama)
+    {
+        nama = iNama;
+    };
+
+    mahasiswa(int iNim, string iNama)
+    {
+        nim = iNim;
+        nama = iNama;
+    };
+
+    void cetak()
+    {
+        cout << "NIM = " << nim << endl;
+        cout << "Nama = " << nama << endl;
+        cout << endl;
+    };
 };
+
+int main()
+{
+    mahasiswa mhs1;
+    mahasiswa mhs2(20);
+    mahasiswa mhs3("Sydney Sweeney");
+    mahasiswa mhs4(30, "Zendaya");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+
+    return 0;
+}
